2600910f8d553919b99eaa43a4d54ebfe769202a
inklingboi <inklingboiii1@gmail.com>
2023-12-04 16:28:47 +0100

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [svelte()]
+  integrations: [svelte()],
+  adapter: cloudflare()
 });
