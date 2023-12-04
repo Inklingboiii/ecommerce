a4bb1681eb2cbb5e41f0870b79de1a60c254e95e
inklingboi <inklingboiii1@gmail.com>
2023-12-04 16:07:16 +0100

    added details page

---
+++ src/api/elephantsql.ts
@@ -0,0 +1,83 @@
+import * as pg from 'pg';
+import type { APIContext, APIRoute } from 'astro'
+export const GET_PRODUCTS: APIRoute = async(context: APIContext) => {
+  const { limit } = context.params;
+  const client = new pg.Client(import.meta.env.CONNECTION_STRING);
+  await client.connect();
+  try {
+  const res = await client.query(`SELECT * FROM "public"."products" LIMIT ${limit}`);
+  return new Response(
+    JSON.stringify({
+      products: res.rows,
+      status: 200
+    })
+  );
+  }
+  catch(err) {
+    console.error(err);
+    JSON.stringify({
+      greeting: err,
+      status: 500
+    })
+  }
+  finally {
+    client.end();
+  }
+  client.end();
+}
+
+export const GET_PRODUCT: APIRoute = async(context: APIContext) => {
+  const { id } = context.params;
+  console.log('id', id)
+  const client = new pg.Client(import.meta.env.CONNECTION_STRING);
+  await client.connect();
+  try {
+  const res = await client.query(`SELECT * FROM "public"."products" WHERE id = '${id}'`);
+  console.log('res', res)
+  return new Response(
+    JSON.stringify({
+      product: res.rows[0],
+      status: 200
+    })
+  );
+  }
+  catch(err) {
+    console.error(err);
+    JSON.stringify({
+      greeting: err,
+      status: 500
+    })
+  }
+  finally {
+    client.end();
+  }
+  client.end();
+}
+
+export const GET_CATEGORY: APIRoute = async(context: APIContext) => {
+  const { category_id } = context.params;
+  console.log('category id', category_id)
+  const client = new pg.Client(import.meta.env.CONNECTION_STRING);
+  await client.connect();
+  try {
+  const res = await client.query(`SELECT * FROM "public"."categories" WHERE id = '${category_id}'`);
+  console.log('res', res)
+  return new Response(
+    JSON.stringify({
+      category: res.rows[0],
+      status: 200
+    })
+  );
+  }
+  catch(err) {
+    console.error(err);
+    JSON.stringify({
+      greeting: err,
+      status: 500
+    })
+  }
+  finally {
+    client.end();
+  }
+  client.end();
+}
---
+++ src/components/ProductList.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let products
+    console.log(products)
+</script>
+<ul>
+    {#each products as product}
+        <li>
+            <img src={`https://unsplash.it/${Math.floor(Math.random() * (2000 - 500 + 1) + 500)}/${Math.floor(Math.random() * (1500 - 500 + 1) + 500)}`} alt="">
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>€{product.price}</p>
+            <a href={`/products/${product.id}`}>More Info</a>
+        </li>
+    {/each}
+</ul>
+
+<style>
+    ul {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(24ch, 1fr));
+		gap: 2rem;
+		padding: 0;
+    }
+
+    ul > li {
+        list-style-type: none;
+        background-color: var(--color-gray);
+        padding: 1em;
+    }
+</style>
