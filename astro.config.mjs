d9da9f8d5ff7ae0b4e4c96082265c701140b00bd
inklingboi <inklingboiii1@gmail.com>
2023-12-09 19:18:54 +0100

    tryput vercel instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
-import cloudflare from "@astrojs/cloudflare";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   integrations: [svelte()],
-  adapter: cloudflare(),
-  experimental: {
-    devOverlay: true,
-  }
+  adapter: vercel(),
+  devOverlay: true,
 });
